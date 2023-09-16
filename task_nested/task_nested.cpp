e0997a377848d9e8e41372e876bdf97a906f2fa9
panterrich <durnov.an@phystech.edu>
2023-09-16 19:35:42 +0300

    FEATURE: add global thread num in task_nested

--- task_nested/task_nested.cpp
+++ task_nested/task_nested.cpp
@@ -3,13 +3,14 @@
 
 static constexpr unsigned int kNumThreads = 2;
 
-void Dump(int level, int prevNumThreads)
+void Dump(int level, int prevNumThreads, int globalNumThread)
 {
     #pragma omp ordered
     {
-        printf("Level %2d: prev num threads %d (%2d/%d)\n",
+        printf("Level %2d: prev num threads %d (%2d/%2d): %2d\n",
                 level, prevNumThreads,
-                omp_get_thread_num(), omp_get_num_threads());
+                omp_get_thread_num(), omp_get_num_threads(),
+                globalNumThread);
     }
 }
 
@@ -18,30 +19,41 @@ int main()
     omp_set_dynamic(false);
     omp_set_nested(true);
 
-    Dump(0, 0);
+    int globalNumThread = 0;
+
+    Dump(0, 0, globalNumThread + (1 << 0));
 
     int level0 = 1;
 
-    #pragma omp parallel for ordered num_threads(kNumThreads)
+    #pragma omp parallel for ordered firstprivate(globalNumThread) num_threads(kNumThreads)
     for (int i = 0; i < omp_get_num_threads(); i++)
     {
         int level1 = omp_get_num_threads();
 
-        Dump(1, level0);
+        globalNumThread += omp_get_thread_num();
 
-        #pragma omp parallel for ordered num_threads(kNumThreads)
+        Dump(1, level0, globalNumThread + (1 << 1));
+
+        globalNumThread <<= 1;
+
+        #pragma omp parallel for ordered firstprivate(globalNumThread) num_threads(kNumThreads)
         for (int j = 0; j < omp_get_num_threads(); j++)
         {
             int level2 = omp_get_num_threads();
 
-            Dump(2, level1);
+            globalNumThread += omp_get_thread_num();
 
-            #pragma omp parallel for ordered num_threads(kNumThreads)
+            Dump(2, level1, globalNumThread + (1 << 2));
+
+            globalNumThread <<= 1;
+
+            #pragma omp parallel for ordered firstprivate(globalNumThread) num_threads(kNumThreads)
             for (int k = 0; k < omp_get_num_threads(); k++)
             {
-                Dump(3, level2);
+                globalNumThread += omp_get_thread_num();
+
+                Dump(3, level2, globalNumThread + (1 << 3));
             }
         }
     }
 }
-
